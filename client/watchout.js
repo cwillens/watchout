3351c21c19eaff8bc5533b3ae1e8f527d917c185
cwillens <charwillens@gmail.com>
2016-09-23 15:58:32 -0700

    added player circle that doesn't drag yet

--- client/watchout.js
+++ client/watchout.js
@@ -16,7 +16,70 @@ var axes = {
 var gameBoard = d3.select('.board').append('svg')
               .attr('width', gameOptions.width)
               .attr('height', gameOptions.height);
+//===============================PLAYER =====================================
+var svg = d3.select('svg');
 
+// DATA JOIN
+// Join new data with old elements, if any.
+var player = svg.selectAll('circle.player')
+  .data([{id: 1}], function(d) { return d.id; });
+
+// UPDATE
+// Update old elements as needed.
+player
+    .attr('cx', function(enemy) { return axes.x(50); } )
+    .attr('cy', function(enemy) { return axes.y(50); } );
+
+// ENTER
+// Create new elements as needed.
+//
+// ENTER + UPDATE
+// After merging the entered elements with the update selection,
+// apply operations to both.
+player.enter().append('circle')
+    .attr('class', 'player')
+    .attr('cx', function(enemy) { return axes.x(50); } )
+    .attr('cy', function(enemy) { return axes.y(50); } )
+    .attr('r', 20)
+    .style('fill', 'yellow');
+    //.pieces(function(d) { return d.id; });
+ // .merge(text)
+   // .attr("x", function(d, i) { return i * 32; });
+
+
+// EXIT
+// Remove old elements as needed.
+player.exit().remove();
+
+
+// var click = function() {
+
+//   // Extract the click location\    
+//    var point = d3.mouse(this);
+//    console.log(point);
+//    //var p = {x: point[0], y: point[1] };
+//   var svg = d3.select('svg');
+
+//   // Append a new point
+//   svg.selectAll('circle').append('circle')
+//       // .attr('transform', 'translate(' + p.x + ',' + p.y + ')')
+//       .attr('r', '5')
+//       .attr('class', "dot")
+//       .style("fill", "yellow");
+//       //.call(drag);
+// };
+// click();
+
+// var drag = d3.behavior.drag()
+//     .on("drag", dragmove);
+
+// function dragmove(d) {
+//   var x = d3.event.x;
+//   var y = d3.event.y;
+//   d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
+// }
+
+//===============================ENEMIESssss ================================
 var enemies = [];
 var makeEnemies = function() {
   enemies = [];
@@ -33,7 +96,7 @@ var update = function (data) {
 
   // DATA JOIN
   // Join new data with old elements, if any.
-  var pieces = svg.selectAll('circle')
+  var pieces = svg.selectAll('circle.enemy')
     .data(data, function(d) { return d.id; });
 
   // UPDATE
@@ -65,6 +128,11 @@ var update = function (data) {
   // Remove old elements as needed.
   pieces.exit().remove();
 };
+
+
+
+//DO STUFF
+
 
 makeEnemies();
 
